894661c4af71ebac8470c1e756b7272ab6a2ce1b
JoJoEnterprise <95145417+JoJoProgramming@users.noreply.github.com>
2022-01-24 10:52:40 -0600

    Update settings.py

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n@gx$_)5b6ufxe*c8y-iji#hbekn%r5q)&1cl6s7cz_k+_-faw'
+SECRET_KEY = ####
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
